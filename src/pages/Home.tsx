f28dbc0869e72e4279796116e3b33f766b2223bc
Wilhan Azevedo Dionizio <wilhan.azevedo@gmail.com>
2023-04-03 11:28:37 -0400

    Inicio do Login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import './Home.css'
 export function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <div className="container-home">
+                <div className="container-home-top">
+                    <span className="title">Relogio de Ponto</span>
+                    <span className="subtitle">#1FTSDFA</span>
+                </div>
+            </div>
+
         </div>
     );
 }
